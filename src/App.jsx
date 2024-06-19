2444c57956778c35e64917761d3aa87e493aac23
therajusah <workwithrajukumar@gmail.com>
2024-06-19 14:18:30 +0530

    Navbar created

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <div className='w-full h-screen text-white bg-zinc-900'>
+    <Navbar />
+    </div>
+  )
+}
+
+export default App
