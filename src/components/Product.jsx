eedaa0d6d016fedd8ac57559495d253943d87dac
therajusah <workwithrajukumar@gmail.com>
2024-06-22 20:34:47 +0530

    Updated Product and Products components to add mover function, index prop, and framer-motion animations

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,12 +1,12 @@
 import PropTypes from "prop-types";
 import Button from "./Button";
 
-const Product = ({ val }) => {
+const Product = ({ val, mover, index }) => {
   if (!val) return null;
 
   return (
-    <div className="w-full py-20 pl-12 pr-20 text-white">
-      <div className="flex items-center justify-between max-w-screen-xl mx-auto">
+    <div className="w-full py-20 pl-12 h-[23rem]  pr-20 text-white">
+      <div onMouseEnter={() => {mover(index)}} className="flex items-center justify-between max-w-screen-xl mx-auto">
         <h1 className="text-6xl font-medium capitalize">{val.title}</h1>
         <div className="w-1/3 details">
           <p className="mb-10">{val.description}</p>
@@ -27,6 +27,8 @@ Product.propTypes = {
     live: PropTypes.bool,
     case: PropTypes.bool,
   }).isRequired,
-};
+  mover: PropTypes.func.isRequired,
+  index: PropTypes.number.isRequired,
+}
 
 export default Product;
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { motion } from "framer-motion";
 import Product from "./Product";
 
 const Products = () => {
@@ -32,11 +34,45 @@ const Products = () => {
     },
   ];
 
+  const [pos, setPos] = useState(0);
+
+  const mover = (index) => {
+    setPos(index * 23);
+  };
+
   return (
-    <div className="mt-20">
+    <div className="relative mt-20">
       {products.map((product, index) => (
-        <Product key={index} val={product} />
+        <Product key={index} val={product} mover={mover} index={index} />
       ))}
+      <div className="absolute top-0 w-full h-full pointer-events-none"></div>
+      <motion.div
+        initial={{ y: 0, x: "-50%" }}
+        animate={{ y: pos + `rem` }}
+        transition={{ ease: [0.76, 0, 0.24, 1], duration: 0.5 }}
+        className="window w-[23rem] h-[23rem] bg-white left-[44%] overflow-hidden absolute top-0"
+      >
+        <motion.div
+          animate={{ y: -pos + `rem` }}
+          transition={{ ease: [0.76, 0, 0.24, 1], duration: 0.5 }}
+          className="w-full h-full bg-sky-100"
+        ></motion.div>
+        <motion.div
+          animate={{ y: -pos + `rem` }}
+          transition={{ ease: [0.76, 0, 0.24, 1], duration: 0.5 }}
+          className="w-full h-full bg-sky-200"
+        ></motion.div>
+        <motion.div
+          animate={{ y: -pos + `rem` }}
+          className="w-full h-full bg-sky-300"
+          transition={{ ease: [0.76, 0, 0.24, 1], duration: 0.5 }}
+        ></motion.div>
+        <motion.div
+          animate={{ y: -pos + `rem` }}
+          className="w-full h-full bg-sky-400"
+          transition={{ ease: [0.76, 0, 0.24, 1], duration: 0.5 }}
+        ></motion.div>
+      </motion.div>
     </div>
   );
 };
